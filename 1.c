f74b50138f8cb0d1e947438ad28541c47f15ce7c
GraffT <sanekpanov@gmail.com>
2013-11-19 11:39:55 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -41,6 +41,7 @@ int main(int argc, char** argv)
 	// AP: а где проверки на ошибки всех системных вызовов? write, close и т.п.? исправить!
 	write(fd,"\n",1);
 	get_vector(size,fd);
+	// AP: а где запись матрицы?
 	if (close(fd) < 0) error("cannot close file \n");
 	return 0;
 }
